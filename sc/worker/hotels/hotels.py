7ba2c3ca37a049bdfee1bc2193cd7e2fc7a9fd02
Alex Malyutin <malutin.alex@gmail.com>
2017-11-19 17:02:43 +0300

    Link to official site - Done.

--- sc/worker/hotels/hotels.py
+++ sc/worker/hotels/hotels.py
@@ -42,7 +42,7 @@ def show_xpath(data):
 
 def to_numbers(cur):
     """Helpful function for cleaning HTML string to int value"""
-    if cur=='':
+    if cur == '':
         return 0
     return int(''.join(re.findall("\d+", cur)))
 
@@ -54,6 +54,27 @@ def get_value(it):
     :return:
     """
     return it[0] if len(it) > 0 else ''
+
+
+def get_web(it, d_id):
+    """
+    Special function for getting WEB-address via HTTP-request as mobile device and parsing response headers
+    :param d_id: location id, required for request to server
+    :return: link to the official site
+    """
+    _HEADERS = {
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Connection': 'keep-alive',
+        'Host': 'www.tripadvisor.com',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36'
+    }
+    _url = 'https://www.tripadvisor.com/ShowUrl?&excludeFromVS=true&odc=MobileBusinessListingsUrl&d=' + d_id + '&url=1'
+    if len(it) > 0:
+        _raw_web = requests.post(url=_url, headers=_HEADERS, allow_redirects=False).headers._store['location'][1]
+        return _raw_web.split('?')[0]
+    else:
+        return ''
 
 
 class Address:
@@ -100,7 +121,7 @@ class Contacts:
         self.phone = 0
         self.web = ''
 
-    def parse(self, it):
+    def parse(self, it, d_id):
         """
         Function for parsing HTML-object to splitting values
         :param it: parent HTML-object, includes the contacts information
@@ -111,8 +132,7 @@ class Contacts:
         _st_add = get_value(it[0].xpath('./div[@class="blEntry phone"]/span/text()'))
         self.phone = to_numbers(_st_add)
         # TODO try to parse link to official site...
-        _st_add = get_value(it[0].xpath('./div[@class="blEntry website"]/span/text()'))
-        self.web = str(_st_add)
+        self.web = get_web(it[0].xpath('./div[@class="blEntry website"]/span/text()'), d_id)
 
 
 class PlacesType:
@@ -159,6 +179,7 @@ class AbstractPlace(ABC):
         self.title = ''
         self.address = Address()
         self.contacts = Contacts()
+        self.ID = None
 
     # CONSTANTS
     HEADERS = {
@@ -207,10 +228,15 @@ class Hotel(AbstractPlace):
         _tmp = self.parser.xpath(
             "/html/body[@id='BODY_BLOCK_JQUERY_REFLOW']/div[@id='PAGE']/div[@id='taplc_hr_atf_north_star_nostalgic_0']/div[@class='atf_header_wrapper']/div[@class='atf_header ui_container is-mobile full_width']/div[@id='taplc_location_detail_header_hotels_0']/div[@class='prw_rup prw_common_atf_header_bl headerBL']/div[@class='blRow']")
         _tmp = _tmp if len(_tmp) > 0 else self.parser.xpath('//div[@class="blRow"]')
+        self.ID = str(_tmp[0].xpath('@data-locid')[0])
         self.json = json.loads(self.parser.xpath('//script[@type="application/ld+json"]//text()')[0])
         if len(_tmp) > 0:
             self.address.parse(_tmp[0])
-            self.contacts.parse(_tmp)
+            self.contacts.parse(_tmp, d_id=self.ID)
+        print('fine')
+
+
+# https://www.tripadvisor.com/ShowUrl?&excludeFromVS=true&odc=MobileBusinessListingsUrl&d=543462&url=1
 
 
 class Hotels(PlacesType):
