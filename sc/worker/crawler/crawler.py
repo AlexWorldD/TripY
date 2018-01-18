d37c6d0941cb5425d50565529fbfa6c5ec289a42
Alex Malyutin <malutin.alex@gmail.com>
2018-01-18 19:19:48 +0300

    ..

--- sc/worker/crawler/crawler.py
+++ sc/worker/crawler/crawler.py
@@ -9,26 +9,28 @@ from pathos.multiprocessing import ProcessingPool as Pool
 
 # CONSTANTS
 HEADERS = {
-	'Accept': 'text/javascript, text/html, application/xml, text/xml, */*',
-	'Accept-Encoding': 'gzip,deflate',
-	'Accept-Language': 'en-US,en;q=0.5',
-	'Cache-Control': 'no-cache',
-	'Connection': 'keep-alive',
-	'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-	'Host': 'www.tripadvisor.com',
-	'Pragma': 'no-cache',
-	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36'
-	# 'X-Requested-With': 'XMLHttpRequest'
+    'Accept': 'text/javascript, text/html, application/xml, text/xml, */*',
+    'Accept-Encoding': 'gzip,deflate',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+    'Host': 'www.tripadvisor.com',
+    'Pragma': 'no-cache',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36'
+    # 'X-Requested-With': 'XMLHttpRequest'
 }
 
 COOKIES = {"SetCurrency": "USD"}
+
 
 class Crawler:
     """
     Base structure for entities such as Hotels/Restaurants and et. al.
     Includes common info such as link and numbers of that entity.
     """
-    def __init__(self, url = '', numbers = 0, r_num = 0, path = '', entity = None):
+
+    def __init__(self, url='', numbers=0, r_num=0, path='', entity=None):
         """Create class represents specific entity for city"""
         self.url = 'https://www.tripadvisor.ru' + url
         self.numbers = numbers
@@ -38,24 +40,24 @@ class Crawler:
         self.path = path
         self.entity_constructor = entity;
         # self.entity_crawler = crawler #entity_crawler var contains a constructor of specific entity crawler
-	
+
     def get_links(self, url):
         page_response = requests.get(url=url, headers=HEADERS, cookies=COOKIES)
         parser = ''
         if page_response.status_code == requests.codes.ok:
             parser = html.fromstring(page_response.content)
         else:
-            print('bad response code: %d' %page_response.status_code)
+            print('bad response code: %d' % page_response.status_code)
             return
         # return parser.xpath('//div[contains(@class,"hasDates")]/div[contains(@class,"prw_meta_hsx")]/div[@class="listing"]//div[@class="listing_title"]/a/@href')
         return parser.xpath(self.path)
 
     def get_entity(self, url):
         # entity = self.entity_crawler(url = url)
-        entity = self.entity_constructor(url = url)
+        entity = self.entity_constructor(url=url)
         entity.collect_main_info()
         return entity
-        
+
     def collect_links(self):
         """
         Simple function for collection links to Hotels from different pages of search result
@@ -67,7 +69,7 @@ class Crawler:
             _link_r = '-'.join(self.url.split('-')[2:4])
         except:
             print("Can't split URL:", self.url, " to 2 parts!")
-            
+
         _parts = self.numbers // 30 + 1
         # Parallel version:
         _part_url = []
@@ -78,7 +80,7 @@ class Crawler:
             for it in tqdm(pool.imap_unordered(self.get_links, _part_url, chunksize)):
                 self.links.extend(it)
             pool.close()
-            
+
     def collect_data(self):
         """
         Function for collecting data from provided list of links
@@ -98,7 +100,6 @@ class Crawler:
             self.data = pool.map(self.get_entity, self.links)
             self.data = [entry for entry in self.data if entry is not None]
             pool.close()
-                
+
         download_end = time.time()
         print("Finished crawling:", download_end - download_start, ' s')
-        
